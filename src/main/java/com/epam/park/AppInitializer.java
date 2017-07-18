d9647164e31c34e9f22ce0c57f54003fefb56a57
mikhaylov <mi.mi.mikhaylov97@gmail.com>
2017-07-18 23:37:38 +0300

    1) Now frame of the project can start with correct connection to database and correct server configurations. As a result, one of the controllers will show you simple page with hello world text.

--- src/main/java/com/epam/park/AppInitializer.java
+++ src/main/java/com/epam/park/AppInitializer.java
@@ -1,4 +1,28 @@
 package com.epam.park;
 
-public class AppInitializer {
+import com.epam.park.config.WebConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    private final static String DISPATCHER = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+        servletContext.addListener(new ContextLoaderListener(ctx));
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
 }
--- src/main/java/com/epam/park/config/WebConfig.java
+++ src/main/java/com/epam/park/config/WebConfig.java
@@ -1,4 +1,32 @@
 package com.epam.park.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.epam.park")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
 }
